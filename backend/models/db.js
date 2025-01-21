9a81272b7ffc8bf786ff15d649c0d9904b1e7f08
karungop <karun.gopal@gmail.com>
2025-01-21 18:45:53 -0500

    setting up db and .env

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
+require('dotenv').config(); // Load .env variables
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', // Replace with your DB username
-  password: '', // Replace with your DB password
-  database: 'workday_clone' // Replace with your DB name
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
